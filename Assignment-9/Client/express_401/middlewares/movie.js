d9a527b29dd2fb313c2a6fdbbc11c62cfbb332c8
Sakib <sakib.sr57@gmail.com>
2023-06-01 19:40:29 +0600

    books api

--- Assignment-9/Client/express_401/middlewares/movie.js
+++
@@ -1,8 +0,0 @@
-exports.requiredJSON = (req, res, next) => {
-    if (!req.is("application/json")) {
-        res.json({ msg: "content type must be application/json" })
-    } else {
-
-        next();
-    }
-}
